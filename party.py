b580b794179a328afb7631090128fa8730f268b1
Wooyang Son <sonwy102@gmail.com>
2020-10-19 19:14:41 +0000

    Added doctests

--- party.py
+++ party.py
@@ -8,13 +8,31 @@ app.secret_key = "SECRETSECRETSECRET"
 
 
 def is_mel(name, email):
-    """Is this user Mel?"""
+    """Is this user Mel?
+    
+    >>> is_mel('Mel Melitpolski', 'mel@ubermelon.com')
+    True
+    >>> is_mel('Judith Butler', 'judith@awesome.com')
+    False
+    """
 
     return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
 
 
 def most_and_least_common_type(treats):
     """Given list of treats, return most and least common treat types.
+
+    >>> treats = [
+    ...     {'type': 'dessert'},
+    ...     {'type': 'dessert'},
+    ...     {'type': 'appetizer'},
+    ...     {'type': 'dessert'},
+    ...     {'type': 'appetizer'},
+    ...     {'type': 'drink'},
+    ... ]
+
+    >>> most_and_least_common_type(treats)
+    ('dessert', 'drink')
 
     Return most and least common treat types in tuple of format (most, least).
     """
